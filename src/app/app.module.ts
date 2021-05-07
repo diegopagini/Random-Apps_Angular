e1f9a992ef9349f39f31f58ed41edcbc5eaa0774
Diego Pagini <dq@qbkconsulting.com>
2021-05-07 13:07:34 -0300

    pwa, build & deploy

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,10 +6,12 @@ import { AppComponent } from './app.component';
 
 import { PagesModule } from './pages/pages.module';
 import { SharedModule } from './shared/shared.module';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from '../environments/environment';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, PagesModule, SharedModule],
+  imports: [BrowserModule, AppRoutingModule, PagesModule, SharedModule, ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production })],
   providers: [],
   bootstrap: [AppComponent],
 })
