3f07ca68d76374d0949194fbb33ac906e5f13ea7
Diego Pagini <dq@qbkconsulting.com>
2021-05-08 07:50:52 -0300

    dados app

--- src/app/pages/app-dados/app-dados.component.ts
+++ src/app/pages/app-dados/app-dados.component.ts
@@ -1,15 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-app-dados',
   templateUrl: './app-dados.component.html',
-  styleUrls: ['./app-dados.component.scss']
+  styleUrls: ['./app-dados.component.scss'],
 })
-export class AppDadosComponent implements OnInit {
+export class AppDadosComponent {
+  diceLeft: string = `../../../assets/img/dice1.png`;
+  direRight: string = `../../../assets/img/dice2.png`;
+  number1: number = 1;
+  number2: number = 2;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  rollDice(): void {
+    this.number1 = this.round();
+    this.number2 = this.round();
+    this.diceLeft = `../../../assets/img/dice${this.number1}.png`;
+    this.direRight = `../../../assets/img/dice${this.number2}.png`;
   }
 
+  round() {
+    return Math.round(Math.random() * 5) + 1;
+  }
 }
